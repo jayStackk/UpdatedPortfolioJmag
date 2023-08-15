f5a04004899238e7530f337ad3e1d8332ad05acf
jayStackk <jmagpantay@codestack.co>
2023-08-15 11:15:22 -0700

    taking out Document

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -7,26 +7,9 @@ import Particle from "@/components/skillset/Particle";
 import ResumePic from '../assets/JohnMCSTACK.png';
 import Image from "next/image";
 
+
 const Resume = () => {
-  // const imageRef = useRef(null);
-
-  // // Function to handle image download
-  // const handleImageDownload = () => {
-  //   const canvas = document.createElement("canvas");
-  //   // const image = imageRef.current;
-
-  //   // canvas.width = image.width;
-  //   // canvas.height = image.height;
-
-  //   // const context = canvas.getContext("2d");
-  //   // context.drawImage(image, 0, 0);
-
-  //   const dataUrl = canvas.toDataURL("image/png");
-  //   const link = document.createElement("a");
-  //   link.href = dataUrl;
-  //   link.download = "JohnMCSTACK.png"; // Set the desired download filename
-  //   link.click();
-  // };
+  
 
   return (
 
